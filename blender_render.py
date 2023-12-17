1498e0e1457a2e5a86066653c44b13145472cc48
ltduan <decad3zz@gmail.com>
2023-12-17 22:50:40 +0100

    finish rendering pipeline

--- blender_render.py
+++ blender_render.py
@@ -8,6 +8,7 @@ import json
 import colorsys
 from math import sin, cos, pi
 from tqdm import tqdm
+import glob
 from rb_config import *
 TAU = 2*pi
 
@@ -19,16 +20,22 @@ def main():
     dir_spray_pcd = './spray_pcd/'
     path_envmap= './textures/Skies-001.jpg'
     dir_bunny = './data/models/bunny_final.obj'
+    path_box = './Assets/box.obj'
+    path_curve = './Assets/plane.obj'
+    path_lights = './Assets/lights.obj'
 
     path_video = "./rendering/test.avi"
-    obj_name_list = sorted(os.listdir(dir_mesh))
-    foam_pcd_name_list = sorted(os.listdir(dir_foam_pcd))
-    spray_pcd_name_list = sorted(os.listdir(dir_spray_pcd))
+    obj_name_list = sorted(glob.glob(os.path.join(dir_mesh, "*.obj")))
+    foam_pcd_name_list = sorted(glob.glob(os.path.join(dir_foam_pcd, "*.ply")))
+    spray_pcd_name_list = sorted(glob.glob(os.path.join(dir_spray_pcd, "*.ply")))
     obj_list_num = len(obj_name_list)
     # obj_path = dir_path+obj_name+".obj"
-    obj_path_list = [dir_mesh+obj_name for obj_name in obj_name_list]
-    foam_pcd_path_list = [dir_foam_pcd + foam_pcd_name for foam_pcd_name in foam_pcd_name_list]
-    spray_pcd_path_list = [dir_spray_pcd + spray_pcd_name for spray_pcd_name in spray_pcd_name_list]
+    # obj_path_list = [dir_mesh+obj_name for obj_name in obj_name_list]
+    # foam_pcd_path_list = [dir_foam_pcd + foam_pcd_name for foam_pcd_name in foam_pcd_name_list]
+    # spray_pcd_path_list = [dir_spray_pcd + spray_pcd_name for spray_pcd_name in spray_pcd_name_list]
+    obj_path_list = obj_name_list
+    foam_pcd_path_list = foam_pcd_name_list
+    spray_pcd_path_list = spray_pcd_name_list
     # render_img_path = dir_path+obj_name+"_render_total.png"
     render_img_path = []
     obj_name_list_wo_ext = []
@@ -37,12 +44,15 @@ def main():
     for idx, obj_name in enumerate(obj_name_list):
         # extract fluid mesh name without extension
         fileName = os.path.splitext(obj_name)[0]
+        fileName = fileName.split("/")[-1]
         obj_name_list_wo_ext.append(fileName)
         # extract foam ply name without extension
         pcdName = os.path.splitext(foam_pcd_name_list[idx])[0]
+        pcdName = pcdName.split("/")[-1]
         foam_pcd_name_list_wo_ext.append(pcdName)
         # extract spray ply name without extension
         pcdName = os.path.splitext(spray_pcd_name_list[idx])[0]
+        pcdName = pcdName.split("/")[-1]
         spray_pcd_name_list_wo_ext.append(pcdName)
         render_img_path.append(dir_rendering+fileName+"_render.png")
 
@@ -64,8 +74,8 @@ def main():
     # camera.rotation_euler = (math.radians(62), math.radians(-2.91), math.radians(45.6))
     # camera.location = (13.2098, -2.61762, 11.2616)
     # camera.rotation_euler = (math.radians(43.962), math.radians(0), math.radians(83.495))
-    camera.location = (132.15, -139.62, 10.716)
-    camera.rotation_euler = (math.radians(95.5), math.radians(0), math.radians(33.9))
+    camera.location = (132.151, -139.624, 10.7156)
+    camera.rotation_euler = (math.radians(95.5347), math.radians(-0.000094), math.radians(33.8674))
     
     # Make this the current camera
     bpy.context.scene.camera = camera
@@ -140,22 +150,36 @@ def main():
     spray_mat_links.new(mat.node_tree.nodes['Musgrave Texture'].outputs[0], mat.node_tree.nodes['Color Ramp'].inputs[0])
 
 
-    # # create foam node group
-    # geo = bpy.data.node_groups.new(type = "GeometryNodeTree", name="foam_geo")
-    # geo.nodes.new(type = "GeometryNodeMeshToPoints")
-    # geo.nodes.new(type = "NodeGroupInput")
-    # geo.nodes.new(type = "NodeGroupOutput")
-    # links = geo.links
-    # group_input = geo.nodes["Group Input"].outputs[0]
-    # group_output = geo.nodes["Group Output"].inputs[0]
-    # print(group_input)
-    # print(group_output)
-    # mtp_out = geo.nodes["Mesh to Points"].outputs[0]
-    # print(mtp_out)
-    # mtp_in = geo.nodes["Mesh to Points"].inputs[0]
-    # print(mtp_in)
-    # links.new(group_input, mtp_in)
-    # links.new(group_output, mtp_out)
+    # create box material
+    mat = bpy.data.materials.new(name="boxMaterial")
+    mat.use_nodes = True
+    mat.node_tree.nodes.new(type='ShaderNodeBsdfPrincipled')
+    inp = mat.node_tree.nodes['Material Output'].inputs['Surface']
+    outp = mat.node_tree.nodes['Principled BSDF'].outputs['BSDF']
+    mat.node_tree.links.new(inp,outp)
+
+    # create curve material
+    mat = bpy.data.materials.new(name="curveMaterial")
+    mat.use_nodes = True
+    mat.node_tree.nodes.new(type='ShaderNodeBsdfDiffuse')
+    mat.node_tree.nodes['Diffuse BSDF'].inputs[1].default_value = 1
+    mat.node_tree.nodes.new(type="ShaderNodeTexChecker")
+    mat.node_tree.nodes["Checker Texture"].inputs[3].default_value = 15
+    mat.node_tree.nodes["Checker Texture"].inputs[1].default_value = (0.165, 0.165, 0.165, 1)
+    mat.node_tree.nodes["Checker Texture"].inputs[2].default_value = (0.051, 0.051, 0.051, 1)
+    mat.node_tree.links.new( mat.node_tree.nodes["Checker Texture"].outputs[0], mat.node_tree.nodes['Diffuse BSDF'].inputs[0])
+    inp = mat.node_tree.nodes['Material Output'].inputs['Surface']
+    outp = mat.node_tree.nodes['Diffuse BSDF'].outputs['BSDF']
+    mat.node_tree.links.new(inp,outp)
+
+    # create light material
+    mat = bpy.data.materials.new(name="lightMaterial")
+    mat.use_nodes = True
+    mat.node_tree.nodes.new(type='ShaderNodeEmission')
+    mat.node_tree.nodes["Emission"].inputs[1].default_value = 5
+    inp = mat.node_tree.nodes['Material Output'].inputs['Surface']
+    outp = mat.node_tree.nodes['Emission'].outputs[0]
+    mat.node_tree.links.new(inp,outp)
 
     # create diffuse white material
     mat = bpy.data.materials.new(name="RigidMaterial")
@@ -170,7 +194,7 @@ def main():
     scene = bpy.context.scene
     scene.render.resolution_x = 1920
     scene.render.resolution_y = 1080
-    scene.render.resolution_percentage = 25
+    scene.render.resolution_percentage = 50
     scene.render.engine = 'CYCLES'
     scene.cycles.device = 'CPU'
     bpy.context.scene.cycles.samples = 128
@@ -196,12 +220,12 @@ def main():
     # light.energy = 1
     # light_ob.location = (3.644, 15.456, 12.611)
     # light_ob.rotation_euler = (math.radians(40.5), math.radians(46), math.radians(143))
-    bpy.ops.object.light_add(type='AREA')
-    light_ob = bpy.context.object
-    light = light_ob.data
-    light.energy = 100
-    light.size = 6
-    light_ob.location = (4.5389, -1.18225, 4.27092)
+    # bpy.ops.object.light_add(type='AREA')
+    # light_ob = bpy.context.object
+    # light = light_ob.data
+    # light.energy = 100
+    # light.size = 6
+    # light_ob.location = (4.5389, -1.18225, 4.27092)
 
 
     #scene.render.engine = 'BLENDER_EEVEE'
@@ -238,12 +262,12 @@ def main():
         mtp_out = geo.nodes["Mesh to Points"].outputs[0]
         geo.nodes["Random Value"].inputs[2].default_value = 0.1
         geo.nodes["Random Value"].inputs[3].default_value = 0.4
-        mtp_in = geo.nodes["Mesh to Points"].inputs[0]
         links = geo.links
+        links.new(geo.nodes["Random Value"].outputs[1], geo.nodes["Mesh to Points"].inputs[3])
+        mtp_in = geo.nodes["Mesh to Points"].inputs[0]
         links.new(group_input, mtp_in)
         links.new(set_material.inputs["Geometry"], mtp_out)
         links.new(set_material.outputs["Geometry"], group_output)    
-        links.new(geo.nodes["Random Value"].outputs[0], geo.nodes["Mesh to Points"].inputs[3])
 
         # load spray point cloud
         bpy.ops.import_mesh.ply(filepath = spray_pcd_path_list[i])
@@ -265,13 +289,12 @@ def main():
         mtp_out = geo.nodes["Mesh to Points"].outputs[0]
         geo.nodes["Random Value"].inputs[2].default_value = 0.1
         geo.nodes["Random Value"].inputs[3].default_value = 0.4
-        geo.nodes["Mesh to Points"].inputs[3].default_value = 0.1
-        mtp_in = geo.nodes["Mesh to Points"].inputs[0]
         links = geo.links
+        links.new(geo.nodes["Random Value"].outputs[1], geo.nodes["Mesh to Points"].inputs[3])
+        mtp_in = geo.nodes["Mesh to Points"].inputs[0]
         links.new(group_input, mtp_in)
         links.new(set_material.inputs["Geometry"], mtp_out)
         links.new(set_material.outputs["Geometry"], group_output)    
-        links.new(geo.nodes["Random Value"].outputs[0], geo.nodes["Mesh to Points"].inputs[3])
 
 
         # load water
@@ -287,23 +310,33 @@ def main():
         rigid_all = []
         if i==0:
             # load rigid
-            # with open(rigid_path_list[0], 'r') as load_rigid:
-            #     rigid_dict = json.load(load_rigid)
-            # scalings = rigid_dict['scalings']
-            # rigid_pos = rigid_dict['pos']
-            # rigid_center = rigid_dict['center']
             bpy.ops.import_scene.obj(filepath=rabbit_config_dict['model_path'])
             rigid = bpy.data.objects[rabbit_config_dict['model_name']]
             rigid_all.append(rigid)
             scalings = rabbit_config_dict['model_scale']
             bpy.ops.transform.resize(value=(scale_ratio[0]*scalings, scale_ratio[1]*scalings, scale_ratio[2]*scalings))
-            # rigid.location = ((rigid_pos[0]  - rigid_center[0]*scalings) * scale_ratio[0], 
-            #                 (-rigid_pos[2] - rigid_center[2]*scalings) * scale_ratio[2],
-            #                 (rigid_pos[1] - rigid_center[1]*scalings * scale_ratio[1]))
-            
             rigid.data.materials.clear()
             rigid.data.materials.append(bpy.data.materials["RigidMaterial"])
+            
+            # load box
+            bpy.ops.import_scene.obj(filepath=path_box)
+            box = bpy.data.objects['Cube.001']
+            box.data.materials.clear()
+            box.data.materials.append(bpy.data.materials["boxMaterial"])
 
+            # load lights
+            bpy.ops.import_scene.obj(filepath=path_lights)
+            for j in range(1, 11):
+                light_name = 'top{}'.format(j)
+                light = bpy.data.objects[light_name]
+                light.data.materials.clear()
+                light.data.materials.append(bpy.data.materials["lightMaterial"])
+
+            # load curve
+            bpy.ops.import_scene.obj(filepath=path_curve)
+            curve = bpy.data.objects['curvePlane']
+            curve.data.materials.clear()
+            curve.data.materials.append(bpy.data.materials["curveMaterial"])
 
         bpy.ops.object.select_all(action="DESELECT")
         # render and remove object
@@ -314,16 +347,9 @@ def main():
         bpy.ops.object.delete()
         spray.select_set(True)
         bpy.ops.object.delete()
-        # for j in range(1000):
-        #     foam = foam_all[j]
-        #     foam.select_set(True)
-        #     bpy.ops.object.delete()
-        # for j in range(len(rigid_all)):
-        #     rigid = rigid_all[j]
-        #     rigid.select_set(True)
-        #     bpy.ops.object.delete()
 
-    create_video = False
+
+    create_video = True
     if create_video:
         scene.render.use_sequencer = True
         scene.sequence_editor_create()
